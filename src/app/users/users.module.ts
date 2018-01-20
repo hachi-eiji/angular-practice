0e3872bedf3dbe349179486ac35207384f75bd5e
hachi-eiji <hachiyae@gmail.com>
2018-01-20 16:43:53 +0900

    generate teacher component

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersDirective } from './users.directive';
+import { TeacherComponent } from './teacher/teacher.component';
 
 @NgModule({
   imports: [
     CommonModule,
     UsersRoutingModule
   ],
-  declarations: [UsersDirective]
+  declarations: [UsersDirective, TeacherComponent]
 })
 export class UsersModule { }
