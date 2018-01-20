535d9b72f18843ca8f1fa731df9db32582ae8296
hachi-eiji <hachiyae@gmail.com>
2018-01-20 16:44:20 +0900

    generate student component

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersDirective } from './users.directive';
 import { TeacherComponent } from './teacher/teacher.component';
+import { StudentComponent } from './student/student.component';
 
 @NgModule({
   imports: [
     CommonModule,
     UsersRoutingModule
   ],
-  declarations: [UsersDirective, TeacherComponent]
+  declarations: [UsersDirective, TeacherComponent, StudentComponent]
 })
 export class UsersModule { }
