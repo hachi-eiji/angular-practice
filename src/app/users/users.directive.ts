4c24ac9f6ae9d3d7154257666f134a7bcf044cd0
hachi-eiji <hachiyae@gmail.com>
2018-01-20 16:49:05 +0900

    add viewContainerRef
    refer from users.component.ts for create component

--- src/app/users/users.directive.ts
+++ src/app/users/users.directive.ts
@@ -1,10 +1,11 @@
-import { Directive } from '@angular/core';
+import {Directive, ViewContainerRef} from "@angular/core";
 
 @Directive({
   selector: '[appUsers]'
 })
 export class UsersDirective {
 
-  constructor() { }
+  constructor(public viewContainerRef: ViewContainerRef) {
+  }
 
 }
