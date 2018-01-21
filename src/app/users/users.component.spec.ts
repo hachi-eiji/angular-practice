76695ec49894a0d676d6d57634bc64563d1546ea
hachi-eiji <hachiyae@gmail.com>
2018-01-22 00:56:22 +0900

    use ngIf
    this is simple implementation than dynamic component

--- src/app/users/users.component.spec.ts
+++ src/app/users/users.component.spec.ts
@@ -1,29 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UsersComponent } from './users.component';
-import { DebugElement, NgModule } from '@angular/core';
-import { UsersDirective } from './users.directive';
 import { TeacherComponent } from './teacher/teacher.component';
 import { StudentComponent } from './student/student.component';
-import { By } from '@angular/platform-browser';
-
-@NgModule({
-  entryComponents: [TeacherComponent, StudentComponent]
-})
-class FakeEntryComponent {
-}
 
 describe('UsersComponent', () => {
   let component: UsersComponent;
   let fixture: ComponentFixture<UsersComponent>;
-  let element: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [FakeEntryComponent],
       declarations: [
         UsersComponent,
-        UsersDirective,
         TeacherComponent,
         StudentComponent
       ]
@@ -35,15 +23,10 @@ describe('UsersComponent', () => {
     fixture = TestBed.createComponent(UsersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-    element = fixture.debugElement.query(By.directive(UsersDirective));
   });
 
   it('should create', () => {
-    expect(component.users.viewContainerRef).toContain([TeacherComponent, StudentComponent]);
+    expect(fixture.debugElement.nativeElement.textContent).toMatch(/(teacher|student) work/);
     expect(component).toBeTruthy();
-  });
-
-  it('should have a users', () => {
-    expect(element).toBeTruthy();
   });
 });
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,29 +1,18 @@
-import { AfterViewInit, Component, ComponentFactoryResolver, OnInit, ViewChild } from '@angular/core';
-
-import { StudentComponent } from './student/student.component';
-import { TeacherComponent } from './teacher/teacher.component';
-import { UsersDirective } from './users.directive';
+import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent implements OnInit, AfterViewInit {
-  @ViewChild(UsersDirective) users: UsersDirective;
+export class UsersComponent implements OnInit {
 
   isTeacher = false;
 
-  constructor(private componentFactoryResolver: ComponentFactoryResolver) {
+  constructor() {
   }
 
   ngOnInit() {
     this.isTeacher = Math.random() > 0.5;
-  }
-
-  ngAfterViewInit() {
-    const component = this.isTeacher ? TeacherComponent : StudentComponent;
-    const factory = this.componentFactoryResolver.resolveComponentFactory(component);
-    this.users.viewContainerRef.createComponent(factory);
   }
 }
