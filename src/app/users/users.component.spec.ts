3de68dbfd0e2555a2311da37c07048a9bbced0e5
hachi-eiji <hachiyae@gmail.com>
2018-01-22 00:25:28 +0900

    add test

--- src/app/users/users.component.spec.ts
+++ src/app/users/users.component.spec.ts
@@ -1,14 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UsersComponent } from './users.component';
+import { DebugElement, NgModule } from '@angular/core';
+import { UsersDirective } from './users.directive';
+import { TeacherComponent } from './teacher/teacher.component';
+import { StudentComponent } from './student/student.component';
+import { By } from '@angular/platform-browser';
+
+@NgModule({
+  entryComponents: [TeacherComponent, StudentComponent]
+})
+class FakeEntryComponent {
+}
 
 describe('UsersComponent', () => {
   let component: UsersComponent;
   let fixture: ComponentFixture<UsersComponent>;
+  let element: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UsersComponent ]
+      imports: [FakeEntryComponent],
+      declarations: [
+        UsersComponent,
+        UsersDirective,
+        TeacherComponent,
+        StudentComponent
+      ]
     })
     .compileComponents();
   }));
@@ -17,9 +35,15 @@ describe('UsersComponent', () => {
     fixture = TestBed.createComponent(UsersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    element = fixture.debugElement.query(By.directive(UsersDirective));
   });
 
   it('should create', () => {
+    expect(component.users.viewContainerRef).toContain([TeacherComponent, StudentComponent]);
     expect(component).toBeTruthy();
+  });
+
+  it('should have a users', () => {
+    expect(element).toBeTruthy();
   });
 });
